f0a99da55a7735adfa1a5fc456108d4faeb24e04
Zunayed Mahfuz <zunayedmahfuz@Zunayeds-MacBook-Air.local>
2020-07-14 23:24:44 -0400

    Added video trailer

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,3 @@
-// Slideshow caraousul
-
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-
-
-
-
 const buttonElement = document.querySelector('#search')
 const inputElement = document.querySelector('#inputValue')
 const movieSearchable = document.querySelector('#movies-searchable')
